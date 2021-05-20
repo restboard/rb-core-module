a33e19a8fb92452aedade5e5618c4be658a37383
Emanuele Bertoldi <emanuele.bertoldi@heply.it>
2021-05-20 14:21:35 +0200

    refactor: remove primary key from generated create and update schemas

--- src/rbresource.js
+++ src/rbresource.js
@@ -1,85 +1,102 @@
-import humanizeString from "humanize-string";
+import humanizeString from 'humanize-string'
 import {
   ERR_MISSING_RESOURCE_NAME,
   ERR_MISSING_RESOURCE_DATA_PROVIDER,
-  ERR_INVALID_RESOURCE_DATA_PROVIDER,
-} from "./errors";
-import { RbDataProvider } from "./rbdataprovider";
+  ERR_INVALID_RESOURCE_DATA_PROVIDER
+} from './errors'
+import { RbDataProvider } from './rbdataprovider'
 
-function _createJsonSchema(properties = {}) {
+function _createJsonSchema (properties = {}) {
   return {
-    type: "object",
+    type: 'object',
     properties: {
-      ...properties,
-    },
-  };
+      ...properties
+    }
+  }
 }
 
-function _columnsFromSchema(schema = {}) {
-  const props = schema.properties || {};
+function _columnsFromSchema (schema = {}) {
+  const props = schema.properties || {}
   return Object.keys(props).map((name) => ({
     name,
-    ...props[name],
-  }));
+    ...props[name]
+  }))
 }
 
 export class RbResource {
-  constructor(opts = {}) {
+  constructor (opts = {}) {
     if (!opts.name) {
-      throw new Error(ERR_MISSING_RESOURCE_NAME);
+      throw new Error(ERR_MISSING_RESOURCE_NAME)
     }
     if (!opts.provider) {
-      throw new Error(ERR_MISSING_RESOURCE_DATA_PROVIDER);
+      throw new Error(ERR_MISSING_RESOURCE_DATA_PROVIDER)
     }
     if (!(opts.provider instanceof RbDataProvider)) {
-      throw new Error(ERR_INVALID_RESOURCE_DATA_PROVIDER);
+      throw new Error(ERR_INVALID_RESOURCE_DATA_PROVIDER)
     }
 
-    this.name = opts.name;
-    this.provider = opts.provider;
+    const {
+      name,
+      provider,
+      key,
+      label,
+      icon,
+      displayAttr,
+      stringify,
+      schema,
+      updateSchema,
+      createSchema,
+      columns
+    } = opts
 
-    this.key = opts.key || "id";
-    this.label = opts.label || humanizeString(this.name);
-    this.icon = opts.icon;
-    this.displayAttr = opts.displayAttr;
-    this.stringify = opts.stringify || (data => `${data[this.displayAttr || this.key]}`);
+    this.name = name
+    this.provider = provider
 
-    const _baseJsonSchema = _createJsonSchema();
+    this.key = key || 'id'
+    this.label = label || humanizeString(this.name)
+    this.icon = icon
+    this.displayAttr = displayAttr
+    this.stringify = stringify || (data => `${data[this.displayAttr || this.key]}`)
+
     const _defaultSchema = _createJsonSchema({
-      [this.key]: { type: "integer" },
-    });
-    const _schema =
-      opts.schema || opts.updateSchema || opts.createSchema || _defaultSchema;
-    this.createSchema = opts.createSchema || opts.schema || _baseJsonSchema;
-    this.updateSchema = opts.updateSchema || opts.schema || _baseJsonSchema;
-    this.columns = opts.columns || _columnsFromSchema(_schema);
+      [this.key]: { type: 'integer' }
+    })
+    const _schema = schema || updateSchema || createSchema || _defaultSchema
+
+    const _baseJsonSchema = JSON.parse(JSON.stringify(_schema))
+    delete _baseJsonSchema.properties[this.key]
+
+    this.createSchema = createSchema || _baseJsonSchema
+    this.updateSchema = updateSchema || _baseJsonSchema
+
+    this.columns = columns || _columnsFromSchema(_schema)
   }
 
-  async getMany(params) {
-    return this.provider.getMany(this.name, params);
+  async getMany (params) {
+    return this.provider.getMany(this.name, params)
   }
 
-  async getOne({ id }) {
-    return this.provider.getOne(this.name, { id });
+  async getOne ({ id }) {
+    return this.provider.getOne(this.name, { id })
   }
 
-  async createOne(data) {
-    return this.provider.createOne(this.name, data);
+  async createOne (data) {
+    return this.provider.createOne(this.name, data)
   }
 
-  async updateOne({ id, ...data }) {
-    return this.provider.createOne(this.name, { id, ...data });
+  async updateOne ({ id, ...data }) {
+    return this.provider.createOne(this.name, { id, ...data })
   }
 
-  async deleteOne({ id }) {
-    return this.provider.deleteOne(this.name, { id });
+  async deleteOne ({ id }) {
+    return this.provider.deleteOne(this.name, { id })
   }
 }
 
-export function createResource(opts) {
-  return new RbResource(opts);
+export function createResource (opts) {
+  return new RbResource(opts)
 }
 
 export default {
-  createResource,
-};
+  createResource
+}
